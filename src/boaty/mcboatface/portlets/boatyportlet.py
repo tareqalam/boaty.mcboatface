193f83e23217158bf31d0ebe02d320cc902495d9
tareq <tareq@zitelab.dk>
2023-02-24 11:11:22 +0100

    added portlet

--- src/boaty/mcboatface/portlets/boatyportlet.py
+++ src/boaty/mcboatface/portlets/boatyportlet.py
@@ -19,43 +19,44 @@ import six.moves.urllib.request
 
 
 class IBoatyportletPortlet(IPortletDataProvider):
-    place_str = schema.TextLine(
-        title=_("Name of your place with country code"),
-        description=_("City name along with country code i.e Delhi,IN"),  # NOQA: E501
-        required=True,
-        default="delhi,in",
-    )
+    """"""
+    # place_str = schema.TextLine(
+    #    title=_("Name of your place with country code"),
+    #    description=_("City name along with country code i.e Delhi,IN"),  # NOQA: E501
+    #    required=True,
+    #    default="delhi,in",
+    # )
 
 
 @implementer(IBoatyportletPortlet)
 class Assignment(base.Assignment):
     schema = IBoatyportletPortlet
 
-    def __init__(self, place_str="delhi,in"):
-        self.place_str = place_str.lower()
+    # def __init__(self, place_str="delhi,in"):
+    #    self.place_str = place_str.lower()
 
     @property
     def title(self):
-        return _("Weather of the place")
+        return _("Boaty portlet")
 
 
 class AddForm(base.AddForm):
     schema = IBoatyportletPortlet
     form_fields = field.Fields(IBoatyportletPortlet)
-    label = _("Add Place weather")
-    description = _("This portlet displays weather of the place.")
+    label = _("Add Boaty portlet")
+    description = _("Boaty portlet.")
 
     def create(self, data):
         return Assignment(
-            place_str=data.get("place_str", "delhi,in"),
+            # place_str=data.get("place_str", "delhi,in"),
         )
 
 
 class EditForm(base.EditForm):
     schema = IBoatyportletPortlet
     form_fields = field.Fields(IBoatyportletPortlet)
-    label = _("Edit Place weather")
-    description = _("This portlet displays weather of the place.")
+    label = _("Edit Boaty portlet")
+    description = _("This portlet displays boaty portlet.")
 
 
 class Renderer(base.Renderer):
@@ -77,41 +78,5 @@ class Renderer(base.Renderer):
     def available(self):
         """Show the portlet only if there are one or more elements and
         not an anonymous user."""
-        return not self.anonymous and self._data()
+        return not self.anonymous
 
-    def weather_report(self):
-        self.result = self._data()
-        return self.result["description"]
-
-    def get_humidity(self):
-        return self.result["humidity"]
-
-    def get_pressure(self):
-        return self.result["pressure"]
-
-    @memoize
-    def _data(self):
-        baseurl = "https://query.yahooapis.com/v1/public/yql?"
-        yql_query = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="{0}")'.format(  # NOQA: E501
-            self.data.place_str,
-        )
-        yql_url = (
-            baseurl
-            + six.moves.urllib.parse.urlencode(
-                {"q": yql_query},
-            )
-            + "&format=json"
-        )
-        result = six.moves.urllib.request.urlopen(yql_url).read()
-        data = json.loads(result)
-        result = {}
-        result["description"] = data["query"]["results"]["channel"][
-            "description"
-        ]  # NOQA: E501
-        result["pressure"] = data["query"]["results"]["channel"]["atmosphere"][
-            "pressure"
-        ]  # NOQA: E501
-        result["humidity"] = data["query"]["results"]["channel"]["atmosphere"][
-            "humidity"
-        ]  # NOQA: E501
-        return result
